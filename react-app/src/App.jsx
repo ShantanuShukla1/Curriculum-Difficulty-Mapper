e0589e19a683eed9ec0de4dd0bba533de8de4464
Shantanu Shukla <sshantanu24@vt.edu>
2026-07-08 17:29:36 -0400

    Wire App.jsx to CurriculumMapLive, connect frontend to backend API

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,32 +1,5 @@
-import { useEffect, useState } from 'react'
-import { Box } from "@mui/material";
-import './App.css'
-import Tile from './Tile.jsx'
+import CurriculumMapLive from './CurriculumMapLive'
 
-function App() {
-  const [terms, setTerms] = useState([]);
-
-  useEffect(() => {
-    setTerms([[1,2,3],[4,5,6],[7,8,9]]);
-  }, []);
-
-  return (
-    <Box sx={{
-      display: "grid",
-      justifyContent: "center",
-      alignItems: "center",
-      gridTemplateColumns: "repeat(3, 300px)",
-      gridAutoRows: 200,
-      gap: 2,
-      padding: 4
-    }}>
-      {terms.map((col, colIndex) => col.map((val, rowIndex) => (
-        <Box>
-          <Tile className={`${rowIndex}-${colIndex}`} difficulty={val}/>
-        </Box>
-      )))}
-    </Box>
-  )
+export default function App() {
+  return <CurriculumMapLive />
 }
-
-export default App
