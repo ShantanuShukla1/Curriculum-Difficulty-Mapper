b026656e23eddea8ff16e13a0d389c58006b4127
Shantanu Shukla <sshantanu24@vt.edu>
2026-07-24 14:42:52 -0400

    docker ignore

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://currdiffmap.discovery.cs.vt.edu',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
